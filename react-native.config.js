624652254db85666b56f979eb079bd6300ac80d4
emmyalexjnr <emmyalexjnr@gmail.com>
2025-09-17 11:14:36 +0100

    - feature: built app

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dependencies: {
+      "@react-native-vector-icons/material-design-icons": {
+        platforms: {
+          android: null,
+          ios: null,
+        }
+      },
+    }
+  };
